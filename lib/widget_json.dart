b669eae2fbd774928b3171a7d668d76d8c3cf655
SmithaK <namrathu1@gmail.com>
2021-10-17 15:43:05 +0530

    first commit

--- lib/widget_json.dart
+++ lib/widget_json.dart
@@ -300,13 +300,13 @@ var columnJson = '''
 
 var assetImageJson = '''
 {
-  "type": "Container",
-  "color": "#FFFFFF",
-  "alignment"
-  "child": {
-    "type": "AssetImage",
-    "name": "assets/vip.png"
-  }
+	"type": "Container",
+	"color": "#FFFFFF",
+	"alignment": "center",
+	"child": {
+		"type": "AssetImage",
+		"name": "assets/vip.png"
+	}
 }
 
 ''';
@@ -618,45 +618,40 @@ var pageviewJson = '''
 
 var expandedJson = '''
 {
-  "type": "Row"
-  "crossAxisAlignment": "start",
-  "mainAxisAlignment": "start",
-  "textBaseline": "alphabetic"
-  "textDirection": "ltr",
-  "children":[
-    {
-      "type" : "Text"
-      "data" : "Flutter"
-    }
-    {
-      "type": "RaisedButton",
-      "color": "##FF00FF",
-      "padding": "8,8,8,8",
-      "textColor": "#00FF00",
-      "elevation" : 8.0,
-      "splashColor" : "#00FF00",
-      "child" :
-        "type": "Text"
-        "data": "Widget"
-      }
-    }
-    {
-      "type" : "Text"
-      "data" : "Demo"
-    }
-    {
-      "type" : "Expanded"
-      "child" :
-        "type" : "Container"
-        "color" : "#FFFF00",
-        "alignment" : "center"
-        "child" :
-          "type" : "Text"
-          "data" : "Expanded Widget"
-        }
-      }
-    }
-  ]
+	"type": "Row",
+	"crossAxisAlignment": "start",
+	"mainAxisAlignment": "start",
+	"textBaseline": "alphabetic",
+	"textDirection": "ltr",
+	"children": [{
+		"type": "Text",
+		"data": "Flutter"
+	}, {
+		"type": "RaisedButton",
+		"color": "##FF00FF",
+		"padding": "8,8,8,8",
+		"textColor": "#00FF00",
+		"elevation": 8.0,
+		"splashColor": "#00FF00",
+		"child": {
+			"type": "Text",
+			"data": "Widget"
+		}
+	}, {
+		"type": "Text",
+		"data": "Demo"
+	}, {
+		"type": "Expanded",
+		"child": {
+			"type": "Container",
+			"color": "#FFFF00",
+			"alignment": "center",
+			"child": {
+				"type": "Text",
+				"data": "Expanded Widget"
+			}
+		}
+	}]
 }
 ''';
 
